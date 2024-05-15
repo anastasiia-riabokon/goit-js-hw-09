90c18145c32724e2bf703d22147341d66f49e4a0
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-05-15 13:29:04 +0300

    upd

---
+++ src/js/2-form.js
@@ -0,0 +1,85 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import 'izitoast/dist/css/iziToast.css';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const LS_KEY = 'feedback-form-state';
+
+const ref = {
+  form: document.querySelector('.feedback-form'),
+};
+
+const { form } = ref;
+
+form.addEventListener('input', () => {
+  formData = {
+    email: form.elements.email.value.trim(),
+    message: form.elements.message.value.trim(),
+  };
+
+  addDataToLocalStorage(LS_KEY, formData);
+
+  if (formData.email.trim() !== '' || formData.message.trim() !== '') {
+    const toast = document.querySelector('.iziToast');
+    if (toast) {
+      iziToast.hide({}, toast);
+    }
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (formData.email.trim() === '' || formData.message.trim() === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'Fill please all fields',
+      position: 'topCenter',
+      closeOnEscape: true,
+    });
+  } else {
+    console.log(formData);
+    removeDataFromLocaleStorage(LS_KEY, formData);
+    formData.email = '';
+    formData.message = '';
+    form.reset();
+  }
+});
+
+function refillReload() {
+  const data = getDataFromLocalStorage(LS_KEY) || {};
+  form.elements.email.value = data.email || '';
+  form.elements.message.value = data.message || '';
+}
+
+refillReload();
+
+function addDataToLocalStorage(key, value) {
+  try {
+    const stringifyData = JSON.stringify(value);
+    localStorage.setItem(key, stringifyData);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function getDataFromLocalStorage(key) {
+  try {
+    const lsData = localStorage.getItem(key);
+    return lsData === null ? undefined : JSON.parse(lsData);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function removeDataFromLocaleStorage(key) {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
